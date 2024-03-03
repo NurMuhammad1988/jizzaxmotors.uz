cc12f969b494afdb08ac67d1e2489c7b7e4e2c57
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-03-03 15:30:17 +0500

    ss

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,10 +9,6 @@ import {
 } from "react-icons/fa";
 
 
-// 
-import { DroidSans_Regular400 } from '@expo-google-fonts/droid-sans';
-// 
-
 const FooterLinks = [
     {
         title: "Home",
@@ -150,7 +146,7 @@ const Footer = () => {
                                                 className="cursor-pointer hover:text-primary duration-300"
                                                 key={data.title}
                                             >
-                                                <span style={{ DroidSans_Regular400 }} className="mr-2">
+                                                <span className="mr-2">
                                                     &#11162;
                                                 </span>
                                                 {/* tepadagi shu narsa &#11162; Unicode belgisi deyilarkan buni chapga o'ngga pastga tepaga qilish mumkun ekan kodini o'zgartirib shu kodni ggoglega yozsang har hillari bor ekan turli tuman linki => https://www.compart.com/en/unicode/  */}
